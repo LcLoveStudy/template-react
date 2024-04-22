e8c6388d0b62b0d0d86efb5645c749c3eca92b7a
lichang <2798323033>
2024-04-22 11:21:56 +0800

    add:vite优化

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,31 @@
-import { defineConfig } from 'vite'
-import { fileURLToPath, URL } from 'node:url'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
+import { fileURLToPath } from "url";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-    css: {
-    postcss: {
-      plugins: [tailwindcss, autoprefixer]
-    }
+  esbuild: {
+    // 打包后去掉console，debugger
+    drop: process.env.NODE_ENV === 'production' ? ['console', 'debugger'] : []
   },
-    resolve: {
+  base: process.env.NODE_ENV === 'production' ? './' : '/',
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
+    },
+  },
+  resolve: {
     // 配置别名
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
   },
-})
+  server: {
+    // 开启局域网
+    host: true,
+    port: 1024,
+    open: true
+  }
+});
