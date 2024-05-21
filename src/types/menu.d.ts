b5f009c2e427a6c0eb1ee72d63698436bca0b556
lichang <dotb116393@163.com>
2024-05-21 16:08:10 +0800

    add:使用.d.ts

--- src/types/menu.d.ts
+++ src/types/menu.d.ts
@@ -1,12 +1,7 @@
 declare type MenuItemType = {
-  // 菜单项的名称
-  name: string
-  // 菜单项的路径
-  path: string
-  // 菜单项的图标
-  icon?: string
-  //是否在菜单中隐藏
-  hidden?: boolean
-  // 菜单项的子菜单
-  children?: MenuItemType[]
+  name: string //菜单项的名称
+  path: string //菜单项的路径
+  icon?: string //菜单项的图标
+  hidden?: boolean //是否在菜单中隐藏
+  children?: MenuItemType[] //菜单项的子菜单
 }
