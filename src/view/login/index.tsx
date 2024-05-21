a856aa1f29a83d98a6c01d70a0fb517e26010c95
lichang <dotb116393@163.com>
2024-05-21 11:04:49 +0800

    add:添加登录页面

--- src/view/login/index.tsx
+++ src/view/login/index.tsx
@@ -1,9 +1,51 @@
-const LoginComp = ()=>{
-    return (
-        <>
-        login
-        </>
-    )
-}
+import { Button, Card, Form, FormProps, Input } from "antd";
+import { useNavigate } from "react-router-dom";
+const LoginComp = () => {
+  type FieldType = {
+    username: string;
+    password: string;
+  };
+  const navigate = useNavigate()
+  const onFinish: FormProps<FieldType>["onFinish"] = (values) => {
+    console.log(values)
+    navigate('/',{
+        replace:true
+    })
+  };
+  return (
+    <div className="h-full flex items-center justify-center">
+      <Card title="登录" bordered={false}>
+        <Form
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
+          style={{ maxWidth: 600 }}
+          onFinish={onFinish}
+          autoComplete="off"
+        >
+          <Form.Item<FieldType>
+            label="用户名"
+            name="username"
+            rules={[{ required: true, message: "请输入用户名!" }]}
+          >
+            <Input />
+          </Form.Item>
 
-export default LoginComp
+          <Form.Item<FieldType>
+            label="密码"
+            name="password"
+            rules={[{ required: true, message: "请输入密码!" }]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+            <Button type="primary" htmlType="submit">
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  );
+};
+
+export default LoginComp;
