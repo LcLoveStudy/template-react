8a293b4079dfe52b18517a1e28bd3c2a870e4e9e
lichang <dotb116393@163.com>
2024-05-22 11:03:53 +0800

    add:添加注释

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 
 /**
  * 修改localStorage的hook
+ * setStorageValue修改后自动同步到localStorage,当设置为null时会清空该localStorage的值
  * @param key 在localStorage中的key
  * @param initialValue 初始值，如果localstorage中有值则不会使用这个值
  * @returns storageValue: 存储的值;setStorageValue: 修改localStorage的值,异步方法
