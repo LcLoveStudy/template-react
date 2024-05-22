1a195110d9ed20c460d8801721fc577e2799a28f
lichang <dotb116393@163.com>
2024-05-22 11:19:13 +0800

    update:优化代码

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,25 +1,23 @@
 import { useEffect, useState } from 'react'
 
 /**
- * 修改localStorage的hook
- * setStorageValue修改后自动同步到localStorage,当设置为null时会清空该localStorage的值
- * @param key 在localStorage中的key
- * @param initialValue 初始值，如果localstorage中有值则不会使用这个值
- * @returns storageValue: 存储的值;setStorageValue: 修改localStorage的值,异步方法
+ * 用于管理 localStorage 值的自定义 React Hook，实现了同步更新。
+ * setStorageValue 函数用于更新 localStorage 值，并触发同步更新到组件状态。
+ * 当设置为null时会清空该localStorage的值
+ * @param {string} key - localStorage 中存储值的键。
+ * @param {*} initialValue - 初始值。如果 localStorage 已经包含给定键的值，则忽略此初始值。
+ * @returns {Object} 包含 storageValue 和 setStorageValue 的对象：
+ *    - storageValue: 当前存储在 localStorage 中的值。
+ *    - setStorageValue: 用于异步更新 localStorage 值的函数。
  */
 export const uesLocalStorage = <T>(key: string, initialValue?: T) => {
   const [storageValue, setValue] = useState(() => {
     const item = localStorage.getItem(key)
-    if (item) {
-      return JSON.parse(item)
-    }
-    return initialValue ? initialValue : null
+    return item ? JSON.parse(item) : initialValue ?? null
   })
   const setStorageValue = (value: T) => {
-    return new Promise((resolve) => {
-      setValue(value)
-      resolve(1)
-    })
+    setValue(value)
+    return Promise.resolve(1)
   }
   useEffect(() => {
     if (storageValue) {
@@ -27,7 +25,7 @@ export const uesLocalStorage = <T>(key: string, initialValue?: T) => {
     } else {
       localStorage.removeItem(key)
     }
-  }, [storageValue])
+  }, [storageValue, key])
   return {
     storageValue,
     setStorageValue
